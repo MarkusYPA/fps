e22fbbce320bfcc3e302d8f50844e3d1f63d0e98
Markus grit:lab <markus.amberla@gmail.com>
2025-11-26 13:01:20 +0200

    fix consts

--- src/consts.rs
+++ src/consts.rs
@@ -69,16 +69,7 @@ pub const MINIMAP_BORDER_COLOR: u32 = 0x00FF_FFFF;
 pub const MINIMAP_PLAYER_DOT_RADIUS: usize = 3;
 pub const MINIMAP_PLAYER_ICON_SIZE: f32 = 12.0;
 
-pub const CEILING_COLOR: u32 = 0x00AA_CCFF;
-pub const FLOOR_COLOR: u32 = 0x0055_5555;   // 0x0042_3934 0x0055_5555
-pub const WALL_COLOR_PRIMARY: u32 = 0x008A_7755;
-pub const WALL_COLOR_SECONDARY: u32 = 0x0069_5A41;
-pub const SPRITE_OTHER_PLAYER_WIDTH: f32 = 0.4;
-pub const SPRITE_OTHER_PLAYER_HEIGHT: f32 = 0.7;
-pub const SPRITE_NPC_WIDTH: f32 = 0.2;
-pub const SPRITE_NPC_HEIGHT: f32 = 0.7;
-pub const CAMERA_PLANE_SCALE: f32 = 0.66;
-
+// Gun and combat
 pub const GUN_SCALE: f32 = 1.0;
 pub const GUN_X_OFFSET: usize = 190;
 pub const CROSSHAIR_SCALE: f32 = 0.5;
